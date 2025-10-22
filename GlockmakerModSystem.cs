cb2627f84d19ccad7b9bef05f0f22ac45594ec08
XtreamMB <xtreammb@gmail.com>
2025-10-22 14:47:55 -0400

    Add files via upload

---
+++ GlockmakerModSystem.cs
@@ -0,0 +1,32 @@
+﻿using Glockmaker.source;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace Glockmaker
+{
+    public class GlockmakerModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+
+            api.RegisterItemClass(Mod.Info.ModID + ".bolt-action", typeof(BoltActionItem));
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("glockmaker:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("glockmaker:hello"));
+        }
+
+    }
+}
